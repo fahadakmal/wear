5d9bab8b56ba89f8ae319567be0dfabf60d885a7
Fahad <fahad.akmal@mavrictech.com>
2022-01-01 20:23:20 +0500

    router commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import './App.css';
-import HomePage from './pages/homepage/homepage.component';
+import "./App.css";
+import {  Route } from "react-router-dom";
+
+import HomePage from "./pages/homepage/homepage.component";
+
+import React from "react";
+
+export const HatsPage = () => {
+  return <div>hats ppage</div>;
+};
 
 function App() {
   return (
-    <div className="App">
-     <HomePage />
+    <div className="App"> 
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop/hats" component={HatsPage} />
     </div>
   );
 }
