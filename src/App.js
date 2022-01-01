75c22bbace77d2a5c2c29af9add6b7d290e5a226
Fahad <fahad.akmal@mavrictech.com>
2022-01-02 01:30:23 +0500

    updating our shop page with our collection

--- src/App.js
+++ src/App.js
@@ -2,18 +2,15 @@ import "./App.css";
 import {  Route } from "react-router-dom";
 
 import HomePage from "./pages/homepage/homepage.component";
-
+import ShopPage from "./pages/shop/shop.compnent";
 import React from "react";
 
-export const HatsPage = () => {
-  return <div>hats ppage</div>;
-};
 
 function App() {
   return (
     <div className="App"> 
         <Route exact path="/" component={HomePage} />
-        <Route exact path="/shop/hats" component={HatsPage} />
+        <Route exact path="/shop" component={ShopPage} />
     </div>
   );
 }
